0c83dc280dbfa72dc344d6c0d92802db6c40b366
Aden Collinge <aden.collinge@gmail.com>
2019-03-15 23:51:17 -0400

    style Menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,26 +2,33 @@ import React from "react";
 import { Link } from "gatsby";
 import ReactDOM from "react-dom";
 const style = {
-  position: 'sticky;',
-  top: '0;',
-  transformOrigin: 'top left;',
-  transform: 'rotate(-90deg) translateX(-100%);',
-  width: '100vh;',
-  display: 'flex;',
-  justifyContent: 'space-around;',
+  position: 'sticky',
+  top: '0',
+  transformOrigin: 'top left',
+  transform: 'rotate(-90deg) translateX(-100%)',
+  width: '100vh',
+  display: 'flex',
+  justifyContent: 'space-around',
+}
+
+const menuItemStyle = (inView) => {
+  return inView ? {  transformOrigin: 'top left',
+    transform: 'rotate(90deg) translateX(20%)'} :
+    {}
 }
 
 const Menu = ({ locale, mobile, location, inView}) => {
+
   return (
-    <div style={{style}} className="menu">
-      <Link style={{color: inView === 'GALLERY' ? 'red': 'green'}} to={locale === "en" ? "/about" : "/fr/about"}>
+    <div style={style} className="menu">
+      <Link style={{}} to={locale === "en" ? "/about" : "/fr/about"}>
         <p>{locale === "en" ? "about" : "a propos"}</p>
       </Link>
       <Link to={locale === "en" ? "/" : "/fr"}>
         <p>{locale === "en" ? "home" : "chez nous"}</p>
       </Link>
-      <Link style={{color: inView === 'GALLERY' ? 'red': 'green'}} to={locale === "en" ? "/fr" : "/"}>
-        <p>{locale === "en" ? "fr" : "en"}</p>
+      <Link style={menuItemStyle(inView === 'GALLERY')} to={locale === "en" ? "/fr" : "/"}>
+        <p>{locale === "en" ? "PROJECTS" : "PROJECTS"}</p>
       </Link>
     </div>
   );
