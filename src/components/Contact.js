ad3d4d28e52ab23279d426dd99794401ddb0a1be
Aden Collinge <aden.collinge@gmail.com>
2020-10-01 15:07:32 -0400

    Update instagram address.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -17,7 +17,7 @@ const unContact = ({ posts, inViewport, innerRef, handleInView, html }) => {
             <h2>{isEnglish ? "Email the artist:" : "Envoyez un courriel à l'artiste:"}</h2>
             <a href="mailto:veronica.florence.mockler@gmail.com">veronica.florence.mockler@gmail.com</a>
             <h2>{isEnglish ? "Follow what she's up to:" : "Suivez son travail:"}</h2>
-            <a href="https://www.instagram.com/veronica_rockler" target="_blank">Instagram</a>
+            <a href="https://www.instagram.com/venorica_rockler" target="_blank">Instagram</a>
         </div>
         <img id="contact-image" src={require("../img/13925043_1266555963385118_5536467185885016532_n.jpg")}/>
 
